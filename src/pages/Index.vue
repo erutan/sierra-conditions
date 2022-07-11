b07d43f0e8fb2597f513c2f5370b7ffe473caa28
Carl Uebelhart <carl@erutan.net>
2022-07-11 10:43:48 -0700

    fix link wrapper

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,14 +13,9 @@
 
       <div class="flex flex-wrap justify-center -mx-4">
         
-        <a href="/conditions/breaking/" class="flex flex-col items-center w-full">
-          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
-            Conditions
-          </h3>
           <p class="text-lg">
             A collection of sites &amp; best practices for finding beta on current conditions. Includes information on conditions specific to the <a href="/conditions/breaking">current year</a> (permits, fires bans, etc) as well as general resources to help understand <a href="/conditions/snow">snow & water levels</a>, <a href="/conditions/fire">fire and smoke impacts</a>, <a href="/conditions/roads">road conditions</a>, <a href="/conditions/weather">weather forecasts</a>, where to find <a href="/conditions/trips">trip reports</a>, and general <a href="/conditions/news">news resources</a> for everything in between.
           </p>
-        </a>
         
       </div>
 
